bf8c299d59de170361a00bdd430b564a34db10c5
Ernesto Rountri <ernesto.rountri@gmail.com>
2023-12-01 17:56:33 -0500

    Added the entry "Exercises/Exercise-1/downloads" in .gitignore

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -6,7 +6,7 @@ from zipfile import ZipFile
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip", 
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
