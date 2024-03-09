1c7fb03223d967c1fab58f235882adba477f245e
Ernesto Rountri <ernesto_rountri@icloud.com>
2024-03-09 15:34:23 -0500

    1st attempt to parse the html for the table contents

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -2,9 +2,28 @@ import requests
 import pandas
 from bs4 import BeautifulSoup
 
-
 def main():
     # your code here
+
+    url = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
+
+    #Fetch the web page content
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        #Parse the HTML content
+        soup = BeautifulSoup(response.content,'html.parser')
+
+        #Find all links on the page
+        links = soup.find_all('a')
+
+        #Extract href attribute from each link
+        for link in links:
+            href = link.get('href')
+            print(href)
+    else:
+        print("Failed to fetch the webpage. Status code:", response.status_code)
     pass
 
 
