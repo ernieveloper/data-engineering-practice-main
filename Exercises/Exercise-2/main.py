a8d08085ddda731910434d09aefed6fdd9d707c4
Ernesto Rountri <ernesto.rountri@dell.com>
2024-03-15 11:48:35 -0500

    Getting closer to extracting just the 1st line of the filtered dataframe

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -28,17 +28,26 @@ def main():
                 name = columns[0].text.strip()
                 last_modified = columns[1].text.strip()
                 size = columns[2].text.strip()
-                #size = columns[2].span.text[0].strip('&0.')
-                #description = columns[3].span.text[0].strip('&0.')
                 description = columns[3].text.strip()
+                
                 #Add the new rows to the datafram using a list and then concatenating them to the data fram. 
                 #https://stackoverflow.com/questions/70837397/good-alternative-to-pandas-append-method-now-that-it-is-being-deprecated#:~:text=append%20was%20deprecated%20because%3A%20%22Series,'t%20be
                 df = pd.concat([df,pd.DataFrame.from_records([{'Name': name, 'Last Modified': last_modified, 'Size': size, 'Description': description }])])
-        #print(df.head())
-        #Look for the value 'Last Modified' column value '2024-01-19 10:08'
-        result = df[df['Last Modified'] == '2024-01-19 10:08']
-        print(result)
+                
+                #Setting a progressive index for the dataframe
+                df.reset_index(drop=True, inplace=True)
+        
+        print(df.head())
+        print('')
 
+        #Look for the value 'Last Modified' column value '2024-01-19 10:08'
+        result = df[df['Last Modified'] == '2024-01-19 10:45']
+        print(len(result), "recrords for 2024-01-19 10:45")
+        #print(df.index)
+        print(result)
+        #download_link = url + df.loc[1,'Name']
+        #print(download_link)
+        #print(df.loc['Name'][0])
     else:
         print("Failed to fetch the webpage. Status code:", response.status_code)
     pass
